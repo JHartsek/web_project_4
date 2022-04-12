b2d93b8476fa4c9188bb5b0ec3492dd1be7a97ca
Julia <JLHartsek@gmail.com>
2022-04-12 15:51:06 -0400

    fix: address first set of reviewer comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,8 @@
 const editProfileButton = document.querySelector('.profile__info-edit-button');
-const header = document.querySelector('.header');
-const profile = document.querySelector('.profile');
-const postsGrid = document.querySelector('.posts-grid');
-const footer = document.querySelector('.footer');
+const editProfileForm = document.querySelector('.profile-edit-form');
 
-const popup = document.querySelector('.popup__container');
+const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close-button');
-const saveButton = document.querySelector('.profile-edit-form__save-button');
 
 let profileName = document.querySelector('.profile__info-name').textContent; 
 let profileAboutMe = document.querySelector('.profile__info-descriptor').textContent; 
@@ -14,35 +10,24 @@ let profileAboutMe = document.querySelector('.profile__info-descriptor').textCon
 const nameField = document.querySelector('#name');
 const aboutField = document.querySelector('#about-me');
 
-
 function handleEditProfile () {
-    nameField.setAttribute('placeholder', profileName); 
-    aboutField.setAttribute('placeholder', profileAboutMe);
-    header.classList.add('popup');
-    profile.classList.add('popup');
-    postsGrid.classList.add('popup');
-    footer.classList.add('popup');
-    popup.classList.add('popup__container-opened');
+    nameField.setAttribute('value', profileName); 
+    aboutField.setAttribute('value', profileAboutMe);
+    popup.classList.add('popup_opened');
 }
-
-editProfileButton.addEventListener('click', handleEditProfile); 
 
 function handleClosePopup () {
-    popup.classList.remove('popup__container-opened');
-    header.classList.remove('popup');
-    profile.classList.remove('popup');
-    postsGrid.classList.remove('popup');
-    footer.classList.remove('popup');
+    popup.classList.remove('popup_opened');
 }
-
-closePopupButton.addEventListener('click', handleClosePopup);
 
 function handleSaveProfileChanges (event) {
     event.preventDefault();
     document.querySelector('.profile__info-name').textContent = nameField.value; 
     document.querySelector('.profile__info-descriptor').textContent = aboutField.value; 
+    handleClosePopup(); 
 }
 
-saveButton.addEventListener('click', handleSaveProfileChanges);
+editProfileButton.addEventListener('click', handleEditProfile); 
+closePopupButton.addEventListener('click', handleClosePopup);
+editProfileForm.addEventListener('submit', handleSaveProfileChanges);
 
-console.log(nameField.value);
